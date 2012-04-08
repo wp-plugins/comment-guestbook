2823c0d5a59d35c346a4c728ad82f7f4597b5bce
mibuthu <mibuthu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-08 12:41:49 +0000

    added tag for version 0.0.2
    git-svn-id: https://plugins.svn.wordpress.org/comment-guestbook/tags/0.0.2@528812 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/comments-template.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The custom template for displaying Comments for comment-guestbook plugin.
+ */
+
+require_once( CGB_PATH.'php/options.php' );
+?>
+	<div id="comments">
+	<?php if ( post_password_required() ) : ?>
+		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.' ); ?></p>
+	</div><!-- #comments -->
+	<?php
+			/* Stop the rest of comments.php from being processed,
+			 * but don't kill the script entirely -- we still have
+			 * to fully load the template.
+			 */
+			return;
+		endif;
+	?>
+
+	<?php if ( have_comments() ) : ?>
+   <?php /*
+		<h2 id="comments-title">
+			<?php
+				printf( get_the_title().' Entries:' );
+			?>
+		</h2>
+		*/
+   ?>
+	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-above">
+			<h1 class="assistive-text"><?php _e( 'Comment navigation' ); ?></h1>
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;' ) ); ?></div>
+		</nav>
+		<?php endif; // check for comment navigation ?>
+
+		<ol class="commentlist">
+			<?php
+				/* Loop through and list the comments. Tell wp_list_comments()
+				 * to use the specified function to format the comments.
+				 */
+				wp_list_comments( array( 'callback' => cgb_options::get( 'cgb_clist_comment_callback' ) ) );
+			?>
+		</ol>
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-below">
+			<h1 class="assistive-text"><?php _e( 'Comment navigation' ); ?></h1>
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;' ) ); ?></div>
+		</nav>
+		<?php endif; // check for comment navigation ?>
+
+	<?php
+		/* If there are no comments and comments are closed, let's leave a little note, shall we?
+		 * But we don't want the note on pages or post types that do not support comments.
+		 */
+		elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="nocomments"><?php _e( 'Comments are closed.' ); ?></p>
+	<?php endif; ?>
+	<?php /*comment_form();*/ ?>
+
+</div><!-- #comments -->
---
+++ readme.txt
@@ -0,0 +1,49 @@
+﻿=== Comment Guestbook ===
+Contributors: mibuthu
+Tags: comment, guestbook, site, comments, integrated, shortcode, modify, list, form
+Requires at least: 3.0
+Tested up to: 3.3.1
+Stable tag: 0.0.2
+
+Add a guestbook site which uses the wordpress integrated comments.
+
+
+== Description ==
+
+The purpose of this plugin is to add a guestbook site which uses the wordpress integrated comments. Simply insert the shortcode [comment-guestbook] into a page to enable this plugin. Find a detailed description and some options to modify the output on the admin page under Comments -> Guestbook.
+
+This is a very early version of this plugin, so the options to modify the output are very limited in this version.
+
+
+== Installation ==
+
+The easiest version of installing is to go to the admin page. There you can install new plugins in the menu Plugins -> Add new. Search for "Comment Guestbook" and press "Install now".
+
+If you want to install the plugin manually download the zip-file and extract the files in your wp-content/plugins folder.
+
+
+== Frequently Asked Questions ==
+
+not available yet, will be added in a later release
+
+
+== Screenshots ==
+
+not available yet, will be added in a later release
+
+
+== Changelog ==
+
+= 0.0.2 (2012-04-08) =
+
+* Allow adjustment of comment list output
+
+* Added the options "adjust comment list output" and "comment callback function" on the admin page
+
+= 0.0.1 (2012-04-03) =
+
+* Initial release
+
+== Upgrade Notice ==
+
+not available yet, will be added in a later release
